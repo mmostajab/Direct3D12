a4c60dbcd5d926314d4f6b0013194011094978f1
Morteza Mostajab <mmostajab@gmail.com>
2017-06-10 12:25:46 +0200

    + Display query and format list is added.

--- AdaptersQuery/main.cpp
+++ AdaptersQuery/main.cpp
@@ -37,8 +37,55 @@ void main() {
 		text += L"\n";
 
 		std::clog << std::string(text.begin(), text.end());
-
+		
 		adapterList.push_back(adapter);
 		++adapterCount;
+
+		uint32_t displayCount = 0;
+		IDXGIOutput* output = nullptr;
+		while (adapter->EnumOutputs(displayCount, &output) != DXGI_ERROR_NOT_FOUND) {
+			DXGI_OUTPUT_DESC desc;
+			output->GetDesc(&desc);
+
+			wchar_t idStr[4];
+			_itow_s(displayCount, idStr, 10);
+
+			std::wstring text = L"\tDisplay ";
+			text += idStr;
+			text += L" \t";
+			text += desc.DeviceName;
+			text += L"\n";
+
+			std::clog << std::string(text.begin(), text.end());
+			
+			++displayCount;
+
+			DXGI_FORMAT displayFormat = DXGI_FORMAT_R8G8B8A8_UINT;
+			uint32_t displayModeCount = 0;
+			uint32_t flags = 0;
+			uint32_t modeCount;
+
+			output->GetDisplayModeList(displayFormat, flags, &modeCount, nullptr);
+
+			std::vector<DXGI_MODE_DESC> modeList(modeCount);
+			output->GetDisplayModeList(displayFormat, flags, &modeCount, modeList.data());
+
+			uint32_t modeId = 0;
+			for (auto& m : modeList) {
+				uint32_t n = m.RefreshRate.Numerator;
+				uint32_t d = m.RefreshRate.Denominator;
+
+				std::wstring text = L"\t\tMode ";
+				text += std::to_wstring(modeId);
+				text += L" \t";
+				text += L"Width = " + std::to_wstring(m.Width) + L" Height = " + std::to_wstring(m.Height) + L" Referesh = " + std::to_wstring(n) + L"/" + std::to_wstring(d) + L"\n";
+
+				std::clog << std::string(text.begin(), text.end());
+
+				modeId++;
+			}
+
+		}
+
 	}
 }
